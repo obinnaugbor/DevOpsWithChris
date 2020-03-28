510c1554e43d560918d1f4b477ace13507c9cc10
Obinna Ugbor <obinnaugbor@gmail.com>
2020-03-28 06:46:51 +0100

    First Commit

---
+++ SimpleAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
